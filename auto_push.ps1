db952dddbc07f0bddd7d1968afa2965b7d99df43
MT4ver2-m81-NiEVtwpA <MT4ver2-m81-NiEVtwpA@dc.srv>
2026-02-28 08:19:39 +0900

    fix bug auto_push.ps1, dashboard.html

--- auto_push.ps1
+++ auto_push.ps1
@@ -1,23 +1,34 @@
+# ===== ê›íË =====
+$repoPath = "C:\Users\MT4ver2-m81-NiEVtwpA\top\top"
+$sourceFile1 = "$env:APPDATA\MetaQuotes\Terminal\Common\Files\result_ea1.json"
+$destFile1 = "$repoPath\result_ea1.json"
+$sourceFile2 = "$env:APPDATA\MetaQuotes\Terminal\Common\Files\result_ea2.json"
+$destFile2 = "$repoPath\result_ea2.json"
+
+Set-Location $repoPath
+
 while ($true) {
 
-    # JSONÉRÉsÅ[
-    Copy-Item "C:\Users\MT4ver2-m81-NiEVtwpA\AppData\Roaming\MetaQuotes\Terminal\Common\Files\result_ea1.json" `
-              "C:\Users\MT4ver2-m81-NiEVtwpA\top\top\result_ea1.json" -Force
+    if (Test-Path $sourceFile1) {
 
-    Copy-Item "C:\Users\MT4ver2-m81-NiEVtwpA\AppData\Roaming\MetaQuotes\Terminal\Common\Files\result_ea2.json" `
-              "C:\Users\MT4ver2-m81-NiEVtwpA\top\top\result_ea2.json" -Force
+        # UTF8(BOMÇ»Çµ)Ç≈çƒï€ë∂
+        $content = Get-Content $sourceFile1 -Raw
+        [System.IO.File]::WriteAllText($destFile1, $content, (New-Object System.Text.UTF8Encoding($false)))
 
-    cd C:\Users\MT4ver2-m81-NiEVtwpA\top\top
+        # UTF8(BOMÇ»Çµ)Ç≈çƒï€ë∂
+        $content = Get-Content $sourceFile2 -Raw
+        [System.IO.File]::WriteAllText($destFile2, $content, (New-Object System.Text.UTF8Encoding($false)))
 
-    git pull
+        git add result_ea1.json
+        git add result_ea2.json
 
-    $changed = git status --porcelain
+        $status = git status --porcelain
 
-    if ($changed) {
-        git add .
-        git commit -m "auto update both EA"
-        git push
-        Write-Host "Updated at $(Get-Date)"
+        if ($status) {
+            git commit -m "Auto update result_ea1.json, result_ea2.json"
+            git push
+            Write-Host "Pushed at $(Get-Date)"
+        }
     }
 
     Start-Sleep -Seconds 60
